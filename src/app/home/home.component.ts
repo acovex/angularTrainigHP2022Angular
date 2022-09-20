d05e54e0f998324da3841761e3c396c74b84df3c
Alejandro Coves <alejandro.coves.vives@hp.com>
2022-09-20 10:03:22 +0200

    Changes

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.sass'],
+})
+export class HomeComponent implements OnInit {
+  public numberExample = 0;
+  srcToImage =
+    'https://images.unsplash.com/photo-1617854818583-09e7f077a156?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
